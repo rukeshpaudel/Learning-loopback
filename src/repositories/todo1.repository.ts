b4f26d70b1085e66186ed9425c0a1793d01d012e
Rukesh Paudel <rukeshpaudel@gmail.com>
2022-07-08 13:00:40 +0545

    Added Repository and Data Source

---
+++ src/repositories/todo1.repository.ts
@@ -0,0 +1,17 @@
+import { inject } from "@loopback/core";
+import { DefaultCrudRepository } from "@loopback/repository";
+import { DbDataSource } from "../datasources/db1.datasource";
+import{Task} from '../models/todo1.model';
+
+export class TodoRepository extends DefaultCrudRepository<
+Task,
+typeof Task.prototype.Task_id
+>
+{
+    constructor(
+        @inject('datasources.db') dataSource: DbDataSource
+    )
+    {
+        super(Task, dataSource)
+    }
+}
